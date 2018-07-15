58b283d9cf254c2a43168486669ea1a2ba66d98c
Chris Wilding <hello@chriswilding.co.uk>
2018-07-15 17:00:21 +0100

    Update isbn verifier

--- isbn-verifier/isbn_verifier.exs
+++ isbn-verifier/isbn_verifier.exs
@@ -13,22 +13,17 @@ defmodule ISBNVerifier do
   """
   @spec isbn?(String.t()) :: boolean
   def isbn?(isbn) do
-    digits =
-      isbn
-      |> String.to_charlist()
-      |> Enum.filter(&(&1 in ?0..?9))
-      |> Enum.map(&(&1 - ?0))
-      |> Enum.reverse()
-
-    with_check_digit =
-      if String.ends_with?(isbn, "X") do
-        [10 | digits]
-      else
-        digits
-      end
-
-    valid?(with_check_digit)
+    isbn
+    |> String.to_charlist()
+    |> Enum.filter(&(&1 in ?0..?9))
+    |> Enum.map(&(&1 - ?0))
+    |> Enum.reverse()
+    |> add_check_digit(String.ends_with?(isbn, "X"))
+    |> valid?
   end
+
+  defp add_check_digit(digits, true), do: [10 | digits]
+  defp add_check_digit(digits, _), do: digits
 
   defp valid?(isbn) when length(isbn) == 10 do
     isbn
