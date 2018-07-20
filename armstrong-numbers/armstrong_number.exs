031aeffaa319fa7b40df38dbd7dc1b94541b846f
Chris Wilding <hello@chriswilding.co.uk>
2018-07-20 15:15:58 +0100

    Update armstrong numbers

--- armstrong-numbers/armstrong_number.exs
+++ armstrong-numbers/armstrong_number.exs
@@ -6,21 +6,19 @@ defmodule ArmstrongNumber do
   @spec is_valid?(integer) :: boolean
   def is_valid?(number) do
     number_of_digits = trunc(:math.floor(:math.log10(number) + 1))
-    is_valid?(number, number_of_digits, number_of_digits) === number
+
+    digits(number, number_of_digits)
+    |> Enum.reduce(0, &(&2 + trunc(:math.pow(&1, number_of_digits)))) === number
   end
 
-  defp is_valid?(number, number_of_digits, n, total \\ 0) do
-    if n < 1 do
-      total
-    else
-      nth_digit = trunc(rem(number, trunc(:math.pow(10, n))) / trunc(:math.pow(10, n - 1)))
+  @spec digits(integer, integer) :: [integer]
+  defp digits(number, number_of_digits) do
+    1..number_of_digits
+    |> Enum.map(&nth_digit(number, &1))
+  end
 
-      is_valid?(
-        number,
-        number_of_digits,
-        n - 1,
-        total + trunc(:math.pow(nth_digit, number_of_digits))
-      )
-    end
+  @spec nth_digit(integer, integer) :: integer
+  defp nth_digit(number, n) do
+    trunc(rem(number, trunc(:math.pow(10, n))) / trunc(:math.pow(10, n - 1)))
   end
 end
