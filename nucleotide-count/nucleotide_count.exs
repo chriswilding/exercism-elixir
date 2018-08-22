c5805f039663bdbcf769890b3f4ebe7826ef480b
Chris Wilding <hello@chriswilding.co.uk>
2018-08-22 22:11:38 +0100

    Add nucleotide count

---
+++ nucleotide-count/nucleotide_count.exs
@@ -0,0 +1,41 @@
+defmodule NucleotideCount do
+  @nucleotides [?A, ?C, ?G, ?T]
+
+  @doc """
+  Counts individual nucleotides in a NucleotideCount strand.
+
+  ## Examples
+
+  iex> NucleotideCount.count('AATAA', ?A)
+  4
+
+  iex> NucleotideCount.count('AATAA', ?T)
+  1
+  """
+  @spec count([char], char) :: non_neg_integer
+  def count(strand, nucleotide) do
+    Enum.reduce(
+      strand,
+      0,
+      fn
+        ^nucleotide, acc -> acc + 1
+        _, acc -> acc
+      end
+    )
+  end
+
+  @doc """
+  Returns a summary of counts by nucleotide.
+
+  ## Examples
+
+  iex> NucleotideCount.histogram('AATAA')
+  %{?A => 4, ?T => 1, ?C => 0, ?G => 0}
+  """
+  @spec histogram([char]) :: map
+  def histogram(strand) do
+    Enum.reduce(strand, %{?A => 0, ?T => 0, ?C => 0, ?G => 0}, fn n, counts ->
+      Dict.update(counts, n, 1, fn count -> count + 1 end)
+    end)
+  end
+end
